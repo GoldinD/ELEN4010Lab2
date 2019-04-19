9ad2c4f341fc8fa03262dafbaea65ded2915302a
Dean Goldin <1235950@students.wits.ac.za>
2019-04-20 00:57:55 +0200

    update

--- mainRoutes.js
+++ mainRoutes.js
@@ -2,7 +2,11 @@ let path = require('path');
 let express = require('express'); 
 let mainRouter = express.Router(); 
 
+mainRouter.get('/about', function(req, res){ 
+	res.sendFile(path.join(__dirname, 'views', 'about.html')); 
+	}); 
 mainRouter.get('/', function (req, res) { 
 	res.send('Hello World, I\'m Node.js'); 
 	});
+	
 module.exports = mainRouter;
