f687ff6c4fd7c50e6d2c4433ca3adadcaca22dbd
Dean Goldin <1235950@students.wits.ac.za>
2019-04-20 02:04:47 +0200

    update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 let path = require('path')
 let express = require('express')
 let app = express()
-let mainRouter = require('/Users/Dean/Desktop/SoftwareRepo/Lab2/Section14/mainRoutes.js')
+let mainRoutes = require('mainRoutes.js')
 
-app.use(mainRouter)
+app.use(mainRoutes)
 let port = process.env.PORT || 3000; 
 app.listen(port); 
 console.log("Express server running on port", port);
